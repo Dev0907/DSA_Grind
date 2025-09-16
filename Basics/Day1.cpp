1253b3145f3c9b79322a21d6e22f9a30cbd5dcce
Dev0907 <devparikh200479@gmail.com>
2025-09-16 21:03:00 +0530

    some brush up

--- Basics/Day1.cpp
+++ Basics/Day1.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 int main()
 {
-  cout<<"Hello world";
+  int a;
+  int rem;
+  int sum=0;
+  int total=0;
+  cin>>a;
+  while(a>0){
+    rem=a%10;
+    sum+=rem;
+    total++;
+    a=a/10;
+  }
+  cout<<total;
+  cout<<sum;
+  return 0;
 }
